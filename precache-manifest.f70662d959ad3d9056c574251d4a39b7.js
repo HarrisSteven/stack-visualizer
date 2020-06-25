79abefdd5d68b6f79086dcf8dcd129956172225e
Steven <stevenharris434@gmail.com>
2020-06-24 20:48:18 -0700

    Updates

---
+++ precache-manifest.f70662d959ad3d9056c574251d4a39b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a263ca120e1959812ed507182e90c7a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467152bffaa5c129b795",
+    "url": "/static/css/main.289b5548.chunk.css"
+  },
+  {
+    "revision": "4505ac99f47450b7a3d8",
+    "url": "/static/js/2.f9144d06.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f9144d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467152bffaa5c129b795",
+    "url": "/static/js/main.2a99a8b8.chunk.js"
+  },
+  {
+    "revision": "4fa24aa172af1765b354",
+    "url": "/static/js/runtime-main.9f3be1ce.js"
+  }
+]);
